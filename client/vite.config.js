3e04c7eed2faa6fa1e297bbf362cfbc4afef654a
DMBrar <dmvbrar@gmail.com>
2024-02-20 10:13:26 -0500

    updated root with server.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
-  plugins: [react()],
-})
+ plugins: [react()],
+ build: {
+  manifest: true,
+  rollupOptions: {
+   input: "./src/main.jsx",
+  },
+ },
+});
