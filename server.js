f35cb014b912a6a78ef76650c38cd22571506c2d
DMBrar <dmvbrar@gmail.com>
2024-02-23 09:04:47 -0500

    update server files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const assetsRouter = require("./server/assets-router");
+app.use("/src", assetsRouter);
 app.use("/", express.static(path.join(__dirname, "public")));
 app.get("/api/v1", (req, res) => {
  res.json({
