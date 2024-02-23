7b7e7679271af4c44c3a16f7d5539c108ad84d6c
DMBrar <dmvbrar@gmail.com>
2024-02-23 16:21:09 -0500

    set up server

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const assetsRouter = require("./server/assets-router");
-app.use("/src", assetsRouter);
-app.use("/", express.static(path.join(__dirname, "public")));
-app.get("/api/v1", (req, res) => {
- res.json({
-  project: "React and Express Boilerplate",
-  from: "Vanaldito",
- });
+import config from './config/config.js' 
+import app from './server/express.js'
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to DressStore application." });
 });
-app.get("/*", (_req, res) => {
- res.sendFile(path.join(__dirname, "public", "index.html"));
+app.listen(config.port, (err) => { 
+if (err) {
+console.log(err) 
+}
+console.info('Server started on port %s.', config.port) 
 })
-const { PORT = 5001 } = process.env;
-app.listen(PORT, () => {
- console.log();
- console.log(` App running in port ${PORT}`);
- console.log();
- console.log(` > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
-});
